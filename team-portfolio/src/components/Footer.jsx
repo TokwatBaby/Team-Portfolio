f0128c9147dad070c5d0bb2ffcf79901cd1991b1
TokwatBaby <ic.janharvey.javier@cvsu.edu.ph>
2025-04-24 22:29:33 +0800

    Update Footer.jsx

--- team-portfolio/src/components/Footer.jsx
+++ team-portfolio/src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
 
         <footer className="pt-[2px] bg-[linear-gradient(to_right,_#00FFFF,_#8A2BE2)]">
             <div className="text-center p-3 bg-[linear-gradient(to_top,_#0D0D0D,_#1E1E1E)]">
-                <p className="text-white text-lg font-semibold antialiased">
+                <p className="text-white text-sm sm:text-lg font-semibold antialiased">
                     &copy; {new Date().getFullYear()} DevNexus.
                     <span 
                     class="text-gray-400"> All rights reserved</span>
